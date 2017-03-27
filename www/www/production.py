fd186d60980761191aa8762c7142c1d808f8d4b1
Grant Jenks <contact@grantjenks.com>
2017-03-26 21:15:25 -0700

    Set static url and root

--- www/www/production.py
+++ www/www/production.py
@@ -107,7 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/%sstatic/' % PREFIX
+STATIC_ROOT = os.path.join(BASE_DIR, 'bin', 'static')
 
 
 from hashids import Hashids
